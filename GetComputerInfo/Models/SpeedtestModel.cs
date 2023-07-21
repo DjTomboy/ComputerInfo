415d0e839d0612cf89bd88af4147b8c10d323fe3
Nagy Tamás Attila <nagytamasattila2@gmail.com>
2023-07-21 11:25:31 +0200

    internet sebesség mérése (#5)

---
+++ GetComputerInfo/Models/SpeedtestModel.cs
@@ -0,0 +1,171 @@
+﻿using Newtonsoft.Json;
+
+namespace ComputerInfo.Models
+{
+    internal class SpeedtestModel
+    {
+        [JsonProperty("type")]
+        public string? Type { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [JsonProperty("ping")]
+        public Ping? Ping { get; set; }
+
+        [JsonProperty("download")]
+        public Download? Download { get; set; }
+
+        [JsonProperty("upload")]
+        public Upload? Upload { get; set; }
+
+        [JsonProperty("packetLoss")]
+        public int? PacketLoss { get; set; }
+
+        [JsonProperty("isp")]
+        public string? Isp { get; set; }
+
+        [JsonProperty("interface")]
+        public Interface? Interface { get; set; }
+
+        [JsonProperty("server")]
+        public Server? Server { get; set; }
+
+        [JsonProperty("result")]
+        public Result? Result { get; set; }
+    }
+
+    public class Download
+    {
+        [JsonProperty("bandwidth")]
+        public int Bandwidth { get; set; }
+
+        [JsonProperty("bytes")]
+        public int Bytes { get; set; }
+
+        [JsonProperty("elapsed")]
+        public int Elapsed { get; set; }
+
+        [JsonProperty("latency")]
+        public Latency? Latency { get; set; }
+
+        public string GetBandwidthInMbps()
+        {
+            return $"{Math.Round((double)Bandwidth / 125000, 2)} Mbps";
+        }
+    }
+
+    public class Interface
+    {
+        [JsonProperty("internalIp")]
+        public string? InternalIp { get; set; }
+
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("macAddr")]
+        public string? MacAddr { get; set; }
+
+        [JsonProperty("isVpn")]
+        public bool IsVpn { get; set; }
+
+        [JsonProperty("externalIp")]
+        public string? ExternalIp { get; set; }
+    }
+
+    public class Latency
+    {
+        [JsonProperty("iqm")]
+        public double Iqm { get; set; }
+
+        [JsonProperty("low")]
+        public double Low { get; set; }
+
+        [JsonProperty("high")]
+        public double High { get; set; }
+
+        [JsonProperty("jitter")]
+        public double Jitter { get; set; }
+
+        public string GetIqmString()
+        {
+            return $"{Math.Round(Iqm, 2)} ms";
+        }
+    }
+
+    public class Ping
+    {
+        [JsonProperty("jitter")]
+        public double Jitter { get; set; }
+
+        [JsonProperty("latency")]
+        public double Latency { get; set; }
+
+        [JsonProperty("low")]
+        public double Low { get; set; }
+
+        [JsonProperty("high")]
+        public double High { get; set; }
+
+        public string GetLatencyString()
+        {
+            return $"{Math.Round(Latency, 2)} ms";
+        }
+    }
+
+    public class Result
+    {
+        [JsonProperty("id")]
+        public string? Id { get; set; }
+
+        [JsonProperty("url")]
+        public string? Url { get; set; }
+
+        [JsonProperty("persisted")]
+        public bool Persisted { get; set; }
+    }
+
+    public class Server
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("host")]
+        public string? Host { get; set; }
+
+        [JsonProperty("port")]
+        public int Port { get; set; }
+
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("location")]
+        public string? Location { get; set; }
+
+        [JsonProperty("country")]
+        public string? Country { get; set; }
+
+        [JsonProperty("ip")]
+        public string? Ip { get; set; }
+    }
+
+    internal class Upload
+    {
+        [JsonProperty("bandwidth")]
+        public int Bandwidth { get; set; }
+
+        [JsonProperty("bytes")]
+        public int Bytes { get; set; }
+
+        [JsonProperty("elapsed")]
+        public int Elapsed { get; set; }
+
+        [JsonProperty("latency")]
+        public Latency? Latency { get; set; }
+
+        public string GetBandwidthInMbps()
+        {
+            return $"{Math.Round((double)Bandwidth / 125000, 2)} Mbps";
+        }
+    }
+}
---
+++ GetComputerInfo/Operations.cs
@@ -0,0 +1,55 @@
+﻿using ComputerInfo.Models;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+
+namespace ComputerInfo
+{
+    internal static class Operations
+    {
+        internal static (SpeedtestModel? speedtestResult, Exception? errorMessage) Speedtest()
+        {
+            string speedtestExePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Utils", "speedtest.exe");
+            ProcessStartInfo startInfo = new()
+            {
+                FileName = speedtestExePath,
+                Arguments = "-f json",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                StandardOutputEncoding = Encoding.UTF8,
+                StandardErrorEncoding = Encoding.UTF8,
+                CreateNoWindow = true,
+            };
+
+            try
+            {
+                using var process = Process.Start(startInfo);
+                var output = process?.StandardOutput.ReadToEnd();
+                var error = process?.StandardError.ReadToEnd();
+                process?.WaitForExit();
+
+                if (process?.ExitCode != 0
+                    || output is null)
+                {
+                    throw new Exception(error);
+                }                
+
+                SpeedtestModel? speedtestResult = JsonConvert.DeserializeObject<SpeedtestModel>(output);
+
+                return speedtestResult is null 
+                    ? throw new Exception() 
+                    : (speedtestResult, null);
+            }
+            catch (Exception ex)
+            {
+                return (null, new Exception(ex.Message));
+            }
+        }
+
+        internal static double ConvertBytesToMBits (int bytes)
+        {
+            return Math.Round(Convert.ToDouble(bytes) / 125000, 2);
+        }
+    }
+}
